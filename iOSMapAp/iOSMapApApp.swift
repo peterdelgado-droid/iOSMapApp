fab57e0cad575c3119aafa8030584d510acaa586
Peter Delgado <peterrafaeldelgado@gmail.com>
2023-09-28 11:54:42 +0200

    Initial Commit

---
+++ iOSMapAp/iOSMapApApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSMapApApp.swift
+//  iOSMapAp
+//
+//  Created by Peter Delgado on 28/9/23.
+//
+
+import SwiftUI
+
+@main
+struct iOSMapApApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
